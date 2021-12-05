eabacf53a1de2fe1aa11191b6467f0382610c5a4
Gabriel Alves Quadra <gabrielalves.quadra@gmail.com>
2021-12-05 20:37:34 -0300

    Importação das Rotas

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import express from 'express'
+import { routes } from './routes'
+import './database/config'
 const PORT = Number(process.env.PORT) || 3008
 
 const app = express()
 
 app.use(express.json())
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hellor, World!' })
-})
+app.use(routes)
 
 app.listen(PORT, '0.0.0.0', () =>
   console.log(`Hosteando na ${PORT}`)
 )
+
+export { app }
