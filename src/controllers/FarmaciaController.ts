3feaca26ca32092e37e7ff468f8b65ba79903766
Gabriel Alves Quadra <gabrielalves.quadra@gmail.com>
2021-12-08 13:30:00 -0300

    Corpo da Requisição é valido

--- src/controllers/FarmaciaController.ts
+++ src/controllers/FarmaciaController.ts
@@ -1,14 +1,20 @@
+import { InvalidRequest } from '@errors/InvalidRequest'
 import { FarmaciaI } from '@schemas/Farmacia'
 import CreateFarmaciaService from '@services/CreateFarmaciaService'
 import { NextFunction, Request, Response } from 'express'
 
 class FarmaciaController {
-  async createFarmacia (request: Request, response: Response, next: NextFunction): Promise<Response|void> {
+  private isInvalid (obj: any): obj is FarmaciaI {
+    return !('razao' in obj && 'fantasia' in obj && 'cnpj' in obj)
+  }
+
+  async createFarmacia (request: Request, response: Response, next: NextFunction): Promise<Response | void> {
     try {
       const farmacia: FarmaciaI = request.body
+      if (this.isInvalid(farmacia)) return next(new InvalidRequest())
       const createFarmacia = new CreateFarmaciaService()
       const result = await createFarmacia.execute(farmacia)
-      return response.status(201).json({ result })
+      return response.status(201).json(result)
     } catch (error) {
       return next(error)
     }
