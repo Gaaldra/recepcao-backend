a207267b9d7ee2c3248c91035572c411959ee238
Gabriel Alves Quadra <gabrielalves.quadra@gmail.com>
2021-12-17 12:15:34 -0300

    New Pharmacy Controllers

--- src/controllers/FarmaciaController.ts
+++ src/controllers/FarmaciaController.ts
@@ -1,20 +1,49 @@
 import { InvalidRequest } from '@errors/InvalidRequest'
-import { FarmaciaI } from '@schemas/Farmacia'
-import CreateFarmaciaService from '@services/CreateFarmaciaService'
+import FarmaciaService from '@services/FarmaciaService'
 import { NextFunction, Request, Response } from 'express'
+import { FarmaciaT } from 'src/entity/Farmacia'
 
 class FarmaciaController {
-  private isInvalid (obj: any): obj is FarmaciaI {
-    return !('razao' in obj && 'fantasia' in obj && 'cnpj' in obj)
-  }
-
   async createFarmacia (request: Request, response: Response, next: NextFunction): Promise<Response | void> {
     try {
-      const farmacia: FarmaciaI = request.body
-      if (this.isInvalid(farmacia)) return next(new InvalidRequest())
-      const createFarmacia = new CreateFarmaciaService()
-      const result = await createFarmacia.execute(farmacia)
+      const newFarm: FarmaciaT = request.body
+      const farmaciaService = new FarmaciaService(newFarm)
+      const result = await farmaciaService.create()
       return response.status(201).json(result)
+    } catch (error) {
+      return next(error)
+    }
+  }
+
+  async updateFarmacia (request: Request, response: Response, next: NextFunction): Promise<Response|void> {
+    try {
+      const { razaoSocial, nomeFantasia, cnpj, telefones }: FarmaciaT = request.body
+      const { id } = request.params
+      if (!id) return next(new InvalidRequest('Necessario um id'))
+      const newInfosFarmacia = new FarmaciaService({ razaoSocial, nomeFantasia, cnpj, telefones })
+      newInfosFarmacia.isValid()
+      const result = await newInfosFarmacia.update(id)
+      return response.json(result)
+    } catch (error) {
+      return next(error)
+    }
+  }
+
+  async getAllFarmacias (request: Request, response: Response, next: NextFunction): Promise<Response | void> {
+    try {
+      const result = await FarmaciaService.getFarmacia()
+      return response.json(result)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getOneFarmacia (request: Request, response: Response, next: NextFunction): Promise<Response | void> {
+    try {
+      const { filter } = request.params
+      if (!filter) return next(new InvalidRequest())
+      const result = await FarmaciaService.getFarmacia(filter)
+      return response.json(result)
     } catch (error) {
       return next(error)
     }
