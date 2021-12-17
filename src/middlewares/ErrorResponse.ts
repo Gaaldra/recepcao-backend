3f63809155e3830831de84377d115154040c21ef
Gabriel Alves Quadra <gabrielalves.quadra@gmail.com>
2021-12-17 12:13:32 -0300

    Default Erros

--- src/middlewares/ErrorResponse.ts
+++ src/middlewares/ErrorResponse.ts
@@ -2,5 +2,6 @@ import { NextFunction, Request, Response } from 'express'
 import BaseError from 'src/errors/BaseError'
 
 export default function (err: BaseError, request: Request, response: Response, next: NextFunction) {
+  if (!(err instanceof BaseError)) return response.status(400).json({ message: err.message })
   response.status(err.code).json({ message: err.message })
 }
